f0c5b4f3bf133f81cfde47208ccdbdad3f592d75
hongyeol <napolehong92@naver.com>
2018-11-17 04:18:45 +0900

    인증기능구현

---
+++ app/src/main/java/com/example/firebasehong/models/User.java
@@ -0,0 +1,8 @@
+package com.example.firebasehong.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String uid, email, name, profileUrl;
+}
--- app/src/main/java/com/example/firebasehong/views/LoginActivity.java
+++ app/src/main/java/com/example/firebasehong/views/LoginActivity.java
@@ -1,21 +1,148 @@
 package com.example.firebasehong.views;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import com.example.firebasehong.R;
+import com.example.firebasehong.models.User;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "TAG";
     private View mProgressView;
+    private SignInButton mSignInbtn;
+    private GoogleApiClient mGoogleAPIClient;
+    private GoogleSignInOptions mGoogleSignInoptions;
+
+    private FirebaseAuth mAuth;
+
+    private static final int GOOGLE_LOGIN_OPEN = 100;
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    private FirebaseDatabase mDatabase;
+
+    private DatabaseReference mUserRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mProgressView = (ProgressBar) findViewById(R.id.login_progress);
+        mSignInbtn = (SignInButton)findViewById(R.id.google_sign_in_button);
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mDatabase = FirebaseDatabase.getInstance();
+        mUserRef = mDatabase.getReference("user");
+        mGoogleSignInoptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
+        mGoogleAPIClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        // 실패 시 처리하는 부분/
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API,mGoogleSignInoptions)
+                .build();
+
+        mSignInbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleAPIClient);
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_OPEN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GOOGLE_LOGIN_OPEN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }else{
+
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isComplete()) {
+                            if(task.isSuccessful()){
+                                final FirebaseUser firebaseUser = task.getResult().getUser();
+                                User user = new User();
+                                user.setEmail(firebaseUser.getEmail());
+                                user.setName(firebaseUser.getDisplayName());
+                                user.setUid(firebaseUser.getUid());
+                                if(firebaseUser.getPhotoUrl() != null)
+                                    user.setProfileUrl(firebaseUser.getPhotoUrl().toString()); // Uri 형태이기 때문에 toString을 넣어줘야함.
+
+                                // user에 대한 데이터를 곧장 사용할 수 없고, 모델 클래스를 만들어야함.
+                                mUserRef.child(user.getUid()).setValue(user, new DatabaseReference.CompletionListener() {
+                                    @Override
+                                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                        if (databaseError == null) {
+                                            Bundle eventBundle = new Bundle();
+                                            eventBundle.putString("email", firebaseUser.getEmail());
+                                            mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, eventBundle); //이벤트가 기록이 되는 것을 확인할 수 있다.
+                                        }
+                                    }
+                                }); //setvalue 비동기이기 때문에 완료가 되었는지 확인 필요!
+                                //메세지의 갯수가 몇 개였는지도 알 수가 있네.
+
+
+                            }else{
+                                Snackbar.make(mProgressView,"로그인에 싶패하였습니다.",Snackbar.LENGTH_LONG).show();
+                            }
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
